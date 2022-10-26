89f6f767b7cfb4f1900fc559c665f823496547e3
AlejandraLH <116322044+AlejandraLH@users.noreply.github.com>
2022-10-25 22:20:28 -0500

    Ejercicio 9
    Realiza un código y un diagrama de flujo de una tabla  de multiplicación de cualquier número que desees .

---
+++ Ejercicio9.cs
@@ -0,0 +1,33 @@
+Namespace Tabla_de_multiplicar_2
+{
+
+    Public static class Program
+
+{
+	Public static void Main()
+
+	{
+		Int num=0;
+
+		Int multiplicador=1;
+
+		Console.WriteLine(“ingresar el numero de que deseas multuplicar “);
+
+		Num=int.Parse(Console.ReadLine());
+
+		While(multiplicador<=12)
+
+		{
+			Console.WriteLine(multiplicador+”*”+num+”=”+(multiplicador*num));
+
+			Multiplicador++;
+
+		}
+
+Console.ReadKey();
+
+	}
+
+}
+
+}
